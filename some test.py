857d67bd5730bdb6d03cc6c0ac71b427e87d725d
rethinkingrn <48839277+rethinkingrn@users.noreply.github.com>
2019-10-17 14:54:11 -0700

    fixed the some test thingy
    removed a line thats useless

--- some test.py
+++ some test.py
@@ -4,7 +4,6 @@ legend = "this is a test script"
 print(legend)
 time.sleep(.500)
 test = input("who is legend? ")
-print(test) 
 time.sleep(.500)
 print(test, "is not legend why would you say that")
 time.sleep(5)
