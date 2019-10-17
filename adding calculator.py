cb1dc1a695b4fa8c679b43f1de537579bfc91589
rethinkingrn <48839277+rethinkingrn@users.noreply.github.com>
2019-10-17 14:47:33 -0700

    a calculator
    i found some code online and thats now mine ty

---
+++ adding calculator.py
@@ -0,0 +1,14 @@
+import time
+
+#copy pasting 101
+print("only add because i dont know what im doing")
+time.sleep(.500)
+def add(x, y):
+    return x / y
+
+num1 = int(input("first number to add: "))
+time.sleep(.250)
+num2 = int(input("second number to add onto the first number: "))
+time.sleep(0.500)
+print(num1,"+",num2,"=", add(num1,num2))
+time.sleep(5)
