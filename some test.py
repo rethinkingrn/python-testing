9fc7e0dcb9343e9ba6e2f5a28386721629351743
rethinkingrn <48839277+rethinkingrn@users.noreply.github.com>
2019-10-18 22:49:54 -0700

    changed "some test" to "who is legend" and added stuff

--- some test.py
+++
@@ -1,9 +0,0 @@
-import time
-legend("this is a test script")
-print(legend)
-time.sleep(.500)
-test = input("who is legend? ")
-time.sleep(.500)
-print(test, "is not legend why would you say that")
-time.sleep(1)
-print(test, "is most likely stubborn also so quite a small brain move right there")
---
+++ who is legend.py
@@ -0,0 +1,16 @@
+import time
+
+legend = ("this is a test script")
+print(legend)
+time.sleep(.500)
+question=input("who is legend? ")
+time.sleep(0.500)
+if question=="henry":
+    print("bro your iq must be lower than a rock")
+if question=="matthew":
+    print("you have the highest iq out there")
+    exit()
+time.sleep(.500)
+print(question, "is not legend why would you say that")
+time.sleep(1)
+print(question, "is most likely stubborn also so quite a small brain move right there")
