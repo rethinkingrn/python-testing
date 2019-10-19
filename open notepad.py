d868c17813cc2dd624030b3d5bff85ff5cd85615
rethinkingrn <48839277+rethinkingrn@users.noreply.github.com>
2019-10-18 22:33:43 -0700

    made a script that just opens notepad
    nothing special, still trying to figure out the basics

---
+++ open notepad.py
@@ -0,0 +1,17 @@
+import os
+import time
+
+print("welcome men to a script that only opens notepad")
+print("now you wait 5 seconds")
+time.sleep(1)
+print(4)
+time.sleep(1)
+print(3)
+time.sleep(1)
+print(2)
+time.sleep(1)
+print(1)
+time.sleep(0.500)
+print("opening now")
+time.sleep(1)
+os.system('notepad.exe')
